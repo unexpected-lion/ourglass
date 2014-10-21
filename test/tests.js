e4006d1684e4c5d72db285af9da669562b48680e
KAG <k.gilhooley@gmail.com>
2014-10-21 15:33:21 -0700

    Add scaffolding for unit tests for game, player, particles, spout, funnel

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,11 @@
-// var expect = chai.expect;
+// run in browser by opening SpecRunner.html
+// run in terminal with command "mocha-phantomjs [root]/ourglass/test/SpecRunner.html"
 
-describe('Game', function(){
-
+describe('game', function(){
+  // tests actual game functions but broken bc canvas
   // var testGame = new SimpleGame();
+
+  // working but not actually informative
   var testGame = {c: {collider: true, entities: [0,1,2]} };
 
   it('should create a new game object', function(){
@@ -23,12 +26,14 @@ describe('Game', function(){
 });
 
 
-describe('Player', function(){
-
+describe('player', function(){
+  // test actual game functions but broken bc canvas
   // var testGame = new SimpleGame();
+  // var testPlayer = testGame.c.entities.create(Player, {x:100,y:5});
+  
+  // working but not actually informative
   var testGame = {c: {collider: true, entities: [0,1,2]} };
-  // var testPlayer = new Player(testGame, {x:100,y:5});
-  var testPlayer = testGame.c.entities.create(Player, {x:100,y:5});
+  var testPlayer = new Player(testGame, {x:100,y:5});
  
   it('should initialize a player when called', function(){
     expect(testPlayer).to.exist;
@@ -40,12 +45,25 @@ describe('Player', function(){
     expect(testPlayer.size.y).to.be.above(0);
   })
 
+  it('should respond to a collision event', function(){
+    // fake a collision event here??
+  })
+
+  it('should respond to user keyboard', function(){
+    // fake keyboard events here??
+  })
+
 });
 
-describe('Particle', function(){
+describe('particle', function(){
+  // test actual game functions but broken bc canvas
   // var testGame = new SimpleGame();
+  // var testParticle = testGame.c.entities.create(Particle, {x:5,y:5});
+  
+  // working but not actually informative
   var testGame = {c: {collider: true, entities: [0,1,2]} };
   var testParticle = new Particle(testGame, {x:5,y:5}); 
+  testParticle.center = {x:250, y:0};
 
   it('should initialize a particle when called', function(){
     expect(testParticle).to.exist;
@@ -56,4 +74,29 @@ describe('Particle', function(){
     expect(testParticle.size.x).to.be.above(0);
     expect(testParticle.size.y).to.be.above(0);
   })
+
+  it('should respond to a collision event', function(){
+    // fake a collision event here??
+  })
+
+  it('should move by itself when update function is called', function(){
+    var originalYPosition = testParticle.center.y;
+    testParticle.update();
+    var updatedYPosition = testParticle.center.y;
+    expect(updatedYPosition).to.be.above(originalYPosition);
+  })
 });
+
+describe('spout', function(){
+  it('should generate particles', function(){
+    // fake a spout
+  })
+});
+
+describe('funnel', function(){
+  it('should catch particles', function(){
+    // something impressive
+  })
+});
+
+
