53e51f306a5985ca64c8e6baa08723fe3f17094e
Nick Stefan <NickStefan12@gmail.com>
2014-10-22 14:39:27 -0700

    two players can bounce on same screen

--- js/otherplayer.js
+++ js/otherplayer.js
@@ -6,10 +6,58 @@ var OtherPlayer = function(game, settings) {
   this.size = {x:100,y:5};
   this.color = "#107";
   this.angle = 0;
+  this.particles = this.c.entities.all(Particle);
 };
 
 
 OtherPlayer.prototype = {
+  collision: function(other) {
+  
+    if (this.particles.indexOf(other) !== -1) {  
+      // LEFT slope
+        // -90 to 0
+        // -180 to -270
+        // 90 to 180
+        // 270 to 360
+      // RIGHT slope
+        // -90 to -180
+        // -270 to -360
+        // 0 to 90
+        // 180 to 270
+      var self = this;
+      var angleBwn = function(minRange,maxRange){
+        if (minRange < self.angle && maxRange > self.angle){
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      // paddle slope left
+      if (angleBwn(-90,0) || angleBwn(-270,-180) ||
+          angleBwn(90,180) || angleBwn(270,360)){
+
+        other.center.y += 2;
+        other.center.x = this.getMinX();
+        
+      // paddle slope right
+      } else if (angleBwn(-180,-90) || angleBwn(-360,-270) ||
+        angleBwn(0,90) || angleBwn(180,270)) {
+
+        other.center.y += 2;
+        other.center.x = this.getMaxX();
+
+      // paddle perfectly flat
+      } else {
+        if (this.center.y > other.center.y){
+            other.center.y -= 20;
+        } else {
+          other.center.y += 20;
+        }
+      }
+    }
+
+  },
 
   draw: function(ctx) {
     ctx.fillStyle = this.color;
--- js/player.js
+++ js/player.js
@@ -6,6 +6,7 @@ var Player = function(game, settings) {
   this.size = {x:100,y:5};
   this.color = "#f07";
   this.angle = 0;
+  this.particles = this.c.entities.all(Particle);
 };
 
 
@@ -13,45 +14,47 @@ Player.prototype = {
 
   collision: function(other) {
     
-    // LEFT slope
-      // -90 to 0
-      // -180 to -270
-      // 90 to 180
-      // 270 to 360
-    // RIGHT slope
-      // -90 to -180
-      // -270 to -360
-      // 0 to 90
-      // 180 to 270
-    var self = this;
-    var angleBwn = function(minRange,maxRange){
-      if (minRange < self.angle && maxRange > self.angle){
-        return true;
-      } else {
-        return false;
+    if (this.particles.indexOf(other) !== -1) {
+      // LEFT slope
+        // -90 to 0
+        // -180 to -270
+        // 90 to 180
+        // 270 to 360
+      // RIGHT slope
+        // -90 to -180
+        // -270 to -360
+        // 0 to 90
+        // 180 to 270
+      var self = this;
+      var angleBwn = function(minRange,maxRange){
+        if (minRange < self.angle && maxRange > self.angle){
+          return true;
+        } else {
+          return false;
+        }
       }
-    }
 
-    // paddle slope left
-    if (angleBwn(-90,0) || angleBwn(-270,-180) ||
-        angleBwn(90,180) || angleBwn(270,360)){
+      // paddle slope left
+      if (angleBwn(-90,0) || angleBwn(-270,-180) ||
+          angleBwn(90,180) || angleBwn(270,360)){
 
-      other.center.y += 2;
-      other.center.x = this.getMinX();
-      
-    // paddle slope right
-    } else if (angleBwn(-180,-90) || angleBwn(-360,-270) ||
-      angleBwn(0,90) || angleBwn(180,270)) {
+        other.center.y += 2;
+        other.center.x = this.getMinX();
+        
+      // paddle slope right
+      } else if (angleBwn(-180,-90) || angleBwn(-360,-270) ||
+        angleBwn(0,90) || angleBwn(180,270)) {
 
-      other.center.y += 2;
-      other.center.x = this.getMaxX();
+        other.center.y += 2;
+        other.center.x = this.getMaxX();
 
-    // paddle perfectly flat
-    } else {
-      if (this.center.y > other.center.y){
-          other.center.y -= 20;
+      // paddle perfectly flat
       } else {
-        other.center.y += 20;
+        if (this.center.y > other.center.y){
+            other.center.y -= 20;
+        } else {
+          other.center.y += 20;
+        }
       }
     }
 
