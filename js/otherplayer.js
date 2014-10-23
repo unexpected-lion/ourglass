c15fcf906d8c3702117b160c11b16657b3fc21c0
John Heroy <johnheroy@gmail.com>
2014-10-23 14:47:29 -0400

    OtherPlayer class initializes with random color

--- js/otherplayer.js
+++ js/otherplayer.js
@@ -1,10 +1,14 @@
+var otherColors = ['#B4EF52', '#7A13A8', '#BF64C2', '#6DE911', '#6A8FE7', 
+  '#ACF2AA', '#84EDB9', '#B5334F', '#31CBAA', '#BEE62B', '#7DB5E5', '#A335B1',
+  '#7B5BB6', '#2CB0EC', '#E7996C', '#C52F74', '#60A5B4'];
+
 var OtherPlayer = function(game, settings) {
   this.c = game.c;
   for (var i in settings) {
     this[i] = settings[i];
   }
   this.size = {x:100,y:5};
-  this.color = "#107";
+  this.color = otherColors[Math.floor(Math.random() * otherColors.length)];
   this.angle = 0;
 };
 
